3833eaeee5736d173b6d07891ec908e40ea08b28
ramonsantos <ramonsantos.pe@gmail.com>
2017-09-24 12:53:12 -0300

    Add Sass.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 'use strict';
 
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-gulp.task('default', []);
+gulp.task('sass', function(){
+  return gulp.src('./source/css/sass/*.scss')
+      .pipe(sass.sync({outputStyle: 'expanded'}).on('error', sass.logError))
+      .pipe(gulp.dest('./source/css/_css'));
+});
+
+gulp.task('default', ['sass']);
