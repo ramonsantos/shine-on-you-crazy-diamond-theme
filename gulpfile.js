0166f105674137bf6ee35ebafe580464985f8602
ramonsantos <ramonsantos.pe@gmail.com>
2017-09-26 14:40:42 -0300

    Add style and Sass ajustment.

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var sass = require('gulp-sass');
 gulp.task('sass', function(){
   return gulp.src('./source/css/sass/*.scss')
       .pipe(sass.sync({outputStyle: 'expanded'}).on('error', sass.logError))
-      .pipe(gulp.dest('./source/css/_css'));
+      .pipe(gulp.dest('./source/css'));
 });
 
 gulp.task('default', ['sass']);
