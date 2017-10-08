d680743cce0639cd09bb41b13f66757bf035567f
ramonsantos <ramonsantos.pe@gmail.com>
2017-10-08 11:57:23 -0300

    Transpiling ES6 to ES5.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,23 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
 
-gulp.task('sass', function(){
-  return gulp.src('./source/css/sass/*.scss')
-      .pipe(sass.sync({outputStyle: 'expanded'}).on('error', sass.logError))
-      .pipe(gulp.dest('./source/css'));
-});
+gulp.task('sass', () =>
+    gulp.src('./source/css/sass/*.scss')
+        .pipe(sass.sync({outputStyle: 'expanded'}).on('error', sass.logError))
+        .pipe(gulp.dest('./source/css'))
+);
 
-gulp.task('default', ['sass']);
+gulp.task('babel', () =>
+    gulp.src('./source/js/es6/*.js')
+        .pipe(concat('scripts.js'))
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(gulp.dest('./source/js'))
+);
+
+gulp.task('default', ['sass', 'babel']);
