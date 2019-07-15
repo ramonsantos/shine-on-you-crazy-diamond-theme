14db4af9c4058cf1ca0fea8d5a04b2d1e5a7c675
ramonsantos <ramonsantos.pe@gmail.com>
2019-07-15 18:52:57 -0300

    Update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,23 @@
 'use strict';
 
-const gulp = require('gulp');
-const sass = require('gulp-sass');
+const { src, dest, parallel } = require('gulp');
 const babel = require('gulp-babel');
+const sass = require('gulp-sass');
 const concat = require('gulp-concat');
 
-gulp.task('sass', () =>
-    gulp.src('./source/css/sass/*.scss')
-        .pipe(sass.sync({outputStyle: 'expanded'}).on('error', sass.logError))
-        .pipe(gulp.dest('./source/css'))
-);
+function css() {
+  return src('./source/css/sass/*.scss')
+    .pipe(sass.sync({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(dest('./source/css'))
+}
 
-gulp.task('babel', () =>
-    gulp.src('./source/js/es6/*.js')
-        .pipe(concat('scripts.js'))
-        .pipe(babel({
-            presets: ['env']
-        }))
-        .pipe(gulp.dest('./source/js'))
-);
+function js() {
+  return src('./source/js/es6/*.js')
+    .pipe(concat('scripts.js'))
+    .pipe(babel({presets: ['env']}))
+    .pipe(dest('./source/js'))
+}
 
-gulp.task('default', ['sass', 'babel']);
+exports.js = js;
+exports.css = css;
+exports.default = parallel(css, js);
