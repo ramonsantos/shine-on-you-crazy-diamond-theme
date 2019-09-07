cdce91be023a7818f4bb4b71de866c3df5cd8d17
ramonsantos <ramonsantos.pe@gmail.com>
2019-09-07 11:41:59 -0300

    EditorConfig ajust

--- source/js/es6/_mobileMenu.js
+++ source/js/es6/_mobileMenu.js
@@ -1,16 +1,12 @@
 let toggleMobileMenu = function (event) {
+  let mobileMenu = document.getElementById("mobileMenu");
 
-    let mobileMenu = document.getElementById("mobileMenu");
+  mobileMenu.classList.toggle("mobile-menu-show");
+  mobileMenu.classList.toggle("mobile-menu-hidden");
 
-    mobileMenu.classList.toggle("mobile-menu-show");
-    mobileMenu.classList.toggle("mobile-menu-hidden");
-
-    event.preventDefault();
-
+  event.preventDefault();
 };
 
-document.getElementById('menuBar')
-        .addEventListener('click', toggleMobileMenu);
+document.getElementById('menuBar').addEventListener('click', toggleMobileMenu);
 
-document.getElementById('menuClose')
-        .addEventListener('click', toggleMobileMenu);
+document.getElementById('menuClose').addEventListener('click', toggleMobileMenu);
